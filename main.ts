0c761a71c2385e0da01b1e0e092ae725dadf08ce
Jo Franchetti <jofranchetti@gmail.com>
2025-03-06 15:18:08 +0000

    initial commit

---
+++ main.ts
@@ -0,0 +1,31 @@
+// @ts-types="npm:@types/express@4.17.15"
+
+import express from 'express';
+import data from "./data.json" with { type: "json" };
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Dinosaur API!");
+});
+
+app.get("/api", (req, res) => {
+  res.send(data);
+});
+
+app.get("/api/:dinosaur", (req, res) => {
+  if (req?.params?.dinosaur) {
+    const found = data.find((item) =>
+      item.name.toLowerCase() === req.params.dinosaur.toLowerCase()
+    );
+    if (found) {
+      res.send(found);
+    } else {
+      res.send("No dinosaurs found.");
+    }
+  }
+});
+
+app.listen(8000);
+console.log(`Server is running on http://localhost:8000`);
